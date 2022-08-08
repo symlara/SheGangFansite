76bd9b4e1129df01d8d7490c8293bdbcc93a39d6
Aaliyah <40181569+symlara@users.noreply.github.com>
2022-08-08 15:24:48 -0500

    updated navigation for site

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,12 +1,13 @@
-import React, {useState} from 'react';
-import { Route, Routes } from 'react-router-dom';
-import {Nav, Navbar, Container, Button, NavDropdown} from 'react-bootstrap'
-import {LinkContainer} from 'react-router-bootstrap';
-import Login from './Authentication/Login';
-import logo from '../assets/shegang.png';
-import her from '../assets/her-returns-to-form-on-her-debut-album-back-of-my-mind.jpg'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'tachyons';
 import { ChatState } from '../context/ChatProvider';
+
+
+import logo from '../assets/shegang.png';
 
 
 function Navigation () {
@@ -15,13 +16,53 @@ function Navigation () {
 
 
     return (
-        
-<nav class="flex justify-between bb b--white-10">
+      <Navbar collapseOnSelect expand="lg" variant="dark" style={{backgroundColor: "#000"}}>
+        <Container>
+          <Navbar.Brand href="/">
+          <img className="dib" alt="logo" src={logo} style={{width: 50, height: 50, borderRadius: 8 }}/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto nav-items">
+              <Nav.Link href="/chats">Dashboard</Nav.Link>
+              <Nav.Link href="/meetups/shegang">Meetups</Nav.Link>
+              <Nav.Link href="/">Advise</Nav.Link>
+            </Nav>
+
+             <Nav> 
+             {!user && (
+              <a class="f6 dib white bg-animate hover-bg-gray hover-black no-underline pv2 ph4 br-pill ba b--white-20" href="/login">Login/Signup</a>
+            )}
+             <NavDropdown title="H.E.R." id="collasible-nav-dropdown">
+                <NavDropdown.Item href="/">H.E.R. Updates</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Albums</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Videos
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Song Samples
+                </NavDropdown.Item>
+              </NavDropdown>
+              {/* <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link> */}
+            </Nav> 
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    
+/* <nav class="flex justify-between bb b--white-10">
   <a class="link white-70 hover-white no-underline flex items-center pa3" href="/">
-   <img src={logo} style={{width: 50, height: 50, borderRadius: 8 }}/>
+   <img className="dib" src={logo} style={{width: 50, height: 50, borderRadius: 8 }}/>
   </a>
   <div class="flex-grow pa3 flex items-center">
-    <a class="f6 link dib white dim mr3 mr4-ns" href="/chats">Dashboard</a>
+    <a class="f6 link dib white dim mr3 mr4-ns nav-item" href="/chats">Dashboard</a>
     <a class="f6 link dib white dim mr3 mr4-ns" href="/meetups/shegang">Meetups</a>
     <a class="f6 link dib white dim mr3 mr4-ns" href="/">Advise</a>
     <a class="f6 link dib white dim mr3 mr4-ns" href="/updates/h.e.r.-updates">H.E.R. Updates</a>
@@ -30,7 +71,7 @@ function Navigation () {
         <a class="f6 dib white bg-animate hover-bg-white hover-black no-underline pv2 ph4 br-pill ba b--white-20" href="/login">Login/Signup</a>
     )}
   </div>
-</nav>
+</nav> */
 
     );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'; 
-import './index.css';
+import './globals.scss';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {ChakraProvider} from "@chakra-ui/react";
 import ChatProvider from './context/ChatProvider';
 
